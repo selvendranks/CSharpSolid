528ff2d35327a50a0be37646be4b3391847e40ab
selvendran <selvendranks@gmail.com>
2026-03-22 15:50:12 +0100

    Abstraction

---
+++ Abstraction/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public class EmailService
+    {
+        public void SendEmail()
+        {
+            Connect();
+            Authenticate();
+            Console.WriteLine("Sending email...");
+            Disconnect();
+            
+        }
+
+        private void Connect()
+        {
+            Console.WriteLine("Connecting to email service");
+        }
+
+        private void Authenticate()
+        {
+            Console.WriteLine("Authenticate User");
+        }
+
+        private void Disconnect()
+        {
+            Console.WriteLine("Disconnecting User");
+        }
+    }
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,5 @@
+﻿using Abstraction;
+using System;
+
+EmailService emailService= new EmailService();
+emailService.SendEmail();
