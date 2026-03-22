450dfc5cf4d4f96b714f5bc1302d7d2f5e627343
selvendran <selvendranks@gmail.com>
2026-03-22 15:50:32 +0100

    Encapsulation

---
+++ Encapsulation/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class BankAccount
+    {
+        private decimal _balance;
+
+        public BankAccount(decimal balance)
+        {
+            Deposit(balance);
+        }
+
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Amount must be positive");
+            }
+
+            this._balance += amount;
+        }
+
+        public void Withdraw(decimal amount) 
+            {
+
+            if (amount <= 0) 
+            {
+
+                throw new ArgumentException("Withdraw amount needs to be positive");
+            }
+
+            if (amount > this._balance)
+            {
+                throw new InvalidOperationException("Amount must be less than balance");
+            }
+
+            this._balance -= amount;
+
+            }
+
+
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,14 @@
+﻿using Encapsulation;
+using System;
+
+
+BankAccount bankAccount = new BankAccount(200);
+
+Console.WriteLine(bankAccount.GetBalance());
+
+bankAccount.Withdraw(100);
+Console.WriteLine(bankAccount.GetBalance());
+
+bankAccount.Deposit(300);
+Console.WriteLine(bankAccount.GetBalance());
+
